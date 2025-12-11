82d07e9691c613791d160e433f3c43a6b9c8b80b
perlongqq <perlong@gmail.com>
2025-12-11 13:25:33 +0800

    Update kernel repository URL in jammy.sh

--- config/suites/jammy.sh
+++ config/suites/jammy.sh
@@ -3,7 +3,7 @@
 export RELASE_NAME="Ubuntu 22.04 LTS (Jammy Jellyfish)"
 export RELASE_VERSION="22.04"
 
-export KERNEL_REPO="https://github.com/Lemon1151/linux-rockchip.git"
+export KERNEL_REPO="https://github.com/perlongqq/linux-rockchip.git"
 export KERNEL_BRANCH="jammy"
 export KERNEL_FLAVOR="rockchip"
 
